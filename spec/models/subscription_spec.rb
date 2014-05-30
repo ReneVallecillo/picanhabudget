4275dbb8120fa7a490e90c291333aad998f307cf
Rene Vallecillo <reneval@gmail.com>
2014-05-29 21:13:51 -0600

    annotating

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id               :integer          not null, primary key
+#  name             :string(255)
+#  lastname         :string(255)
+#  grade_id         :integer
+#  section_id       :integer
+#  level_id         :integer
+#  paymentmethod_id :integer
+#  school_id        :integer
+#  active           :boolean
+#  fechaingreso     :date
+#  fechainactivo    :date
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 require 'spec_helper'
 
 describe Subscription do
