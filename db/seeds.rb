b7136fe388834f25e2353924a87475a428bbe736
Rene Vallecillo <reneval@gmail.com>
2014-05-29 20:11:37 -0600

    fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,36 @@ user = CreateAdminService.new.call
 puts 'Created Admin User : ' << user.email
 
 
-schools = School.create({name:'Colegio Centro America'})
-schoolings = Schooling.create({name: 'Preescolar'},{name: 'Primaria'},
-  {name: 'Secundaria'})
-levels = Level.create(
-    { name: 'I nivel',scholling:schoolings.first},
-    { name: 'II nivel', scholling:schoolings.first},
-    { name: 'III nivel',scholling:schoolings.first},
-    { name: '1er grado',scholling:schoolings[1] },
-    { name: '2do grado',scholling:schoolings[1] },
-    { name: '3er grado',scholling:schoolings[1] },
-    { name: '4to grado',scholling:schoolings[1] },
-    { name: '5to grado',scholling:schoolings[1] },
-    { name: '6to grado',scholling:schoolings[1] },
-    { name: '1er año',scholling:schoolings.last },
-    { name: '2do año',scholling:schoolings.last },
-    { name: '3er año',scholling:schoolings.last },
-    { name: '4to año',scholling:schoolings.last },
-    { name: '5to año',scholling:schoolings.last }
-  })
+schools = School.create([{name:'Colegio Centro America'}])
+puts 'Created Schools'
+schoolings = Schooling.create([{name: 'Preescolar'},{name: 'Primaria'},
+  {name: 'Secundaria'}])
+puts 'Created Schooling'
 
-sections = Section.create({
+levels = Level.create([
+    { name: 'I nivel',schooling:schoolings.first},
+    { name: 'II nivel', schooling:schoolings.first},
+    { name: 'III nivel',schooling:schoolings.first},
+    { name: '1er grado',schooling:schoolings[1] },
+    { name: '2do grado',schooling:schoolings[1] },
+    { name: '3er grado',schooling:schoolings[1] },
+    { name: '4to grado',schooling:schoolings[1] },
+    { name: '5to grado',schooling:schoolings[1] },
+    { name: '6to grado',schooling:schoolings[1] },
+    { name: '1er año',schooling:schoolings.last },
+    { name: '2do año',schooling:schoolings.last },
+    { name: '3er año',schooling:schoolings.last },
+    { name: '4to año',schooling:schoolings.last },
+    { name: '5to año',schooling:schoolings.last }
+  ])
+puts 'Created Levels'
+
+sections = Section.create([
     {name: 'A'}, {name: 'B'}, {name:'C'}, {name:'D'}, {name:'E'}
-  })
+  ])
+puts 'Created Sections'
 
-paymentmethod = PaymentMethod.create({
+paymentmethod = PaymentMethod.create([
     {name: 'Colegio'}, {name: 'Privado'},{name: 'Gratisasc'}
-  })
+  ])
+puts 'Created Payment Methods'
